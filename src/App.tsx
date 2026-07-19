15aa3180dc1baa3b23f8a0c92feddda300ed01f2
uniqueGbengah <fagbenrogbengah@gmail.com>
2026-07-19 17:11:57 +0100

    Configuring Ts and JSX router and alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+
 function App() {
   return (
     <main className="min-h-screen bg-slate-100 flex items-center justify-center">
       <div className="rounded-xl bg-white p-8 shadow-lg">
-        <h1 className="text-4xl font-bold text-orange-600"> FarmRoute Frontend</h1>
+        <h1 className="text-4xl font-bold text-green-600"> FarmRoute Frontend</h1>
 
         <p className="mt-3 text-gray-600">Tailwind CSS v4 is working.</p>
       </div>
