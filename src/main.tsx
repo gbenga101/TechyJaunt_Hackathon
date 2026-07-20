5ef2f6d0bafb0a246e4c495cdc58b33562605a19
uniqueGbengah <fagbenrogbengah@gmail.com>
2026-07-20 08:39:35 +0100

    Done with Step 11 and React-hot

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
 import "./index.css";
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
+        <Toaster position="top-right" reverseOrder={false} gutter={8} />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
